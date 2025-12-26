76d38e373dea572e0ffc2e123988523b64da8a22
Nehuen Lian <nehuenn.lian@gmail.com>
2025-12-26 06:23:10 -0300

    add scheduler to verify and refresh token every 11 hours

---
+++ service/utils/token_scheduler.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timezone
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+from service.controllers.request_access_token_controller import generate_token
+from service.utils.logger import logger
+from service.xml_management.xml_builder import is_expired, xml_exists
+
+scheduler = AsyncIOScheduler()
+
+async def run_job():
+    logger.info("Starting job: verifying token expiration")
+    
+    if xml_exists("loginTicketResponse.xml"):
+        if is_expired("loginTicketResponse.xml"):
+            await generate_token()
+            return
+        
+    logger.info("Token is still valid. Job finished.")
+
+
+def start_scheduler():
+    logger.info("Scheduler starting: job configured to run every 11 hours")
+
+    scheduler.add_job(
+        run_job,
+        trigger="interval",
+        hours=11,
+        id="afip_token_watchdog",
+        replace_existing=True,
+        max_instances=1,
+        coalesce=True,
+        next_run_time=datetime.now(timezone.utc)
+    )
+    scheduler.start()   
+
+def stop_scheduler():
+    scheduler.shutdown(wait=False)
