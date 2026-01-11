4d2a196ffc1295fea0560528070cae7985a17914
Nehuen Lian <nehuenn.lian@gmail.com>
2026-01-10 21:22:34 -0300

    move client and network logic to the controller.

--- service/controllers/request_access_token_controller.py
+++ service/controllers/request_access_token_controller.py
@@ -1,10 +1,16 @@
+import httpx
+from zeep import AsyncClient
+from zeep.transports import AsyncTransport
+
 from service.crypto.sign import get_binary_cms, sign_login_ticket_request
-from service.soap_client.wsaa import login_cms
+from service.soap_client.wsaa import consult_afip_wsaa
+from service.soap_client.wsdl.wsdl_manager import get_wsaa_wsdl
 from service.time.time_management import generate_ntp_timestamp
 from service.utils.logger import logger
 from service.xml_management.xml_builder import (
     build_login_ticket_request, parse_and_save_loginticketresponse, save_xml)
 
+afip_wsdl = get_wsaa_wsdl()
 
 async def generate_afip_access_token() -> None:
 
@@ -14,7 +20,21 @@ async def generate_afip_access_token() -> None:
     save_xml(root, "loginTicketRequest.xml")
     sign_login_ticket_request()
     b64_cms = get_binary_cms()
-    login_ticket_response = await login_cms(b64_cms)
+
+    async def make_request():
+        try:
+            httpx_client = httpx.AsyncClient(timeout=30.0)
+            transport = AsyncTransport(client=httpx_client)
+            client = AsyncClient(wsdl=afip_wsdl, transport=transport)
+            return await client.service.loginCms(b64_cms)
+
+        finally:
+            if client and client.transport:
+                await client.transport.aclose()
+            else:
+                await httpx_client.aclose()
+
+    login_ticket_response = await consult_afip_wsaa(make_request, "loginCms")
 
     if login_ticket_response["status"] == "success":
         parse_and_save_loginticketresponse(login_ticket_response["response"], save_xml)
--- service/soap_client/wsaa.py
+++ service/soap_client/wsaa.py
@@ -4,9 +4,7 @@ from builtins import ConnectionResetError
 import httpx
 from tenacity import (before_sleep_log, retry, retry_if_exception_type,
                       stop_after_attempt, wait_fixed)
-from zeep import AsyncClient
 from zeep.exceptions import Fault, TransportError, XMLSyntaxError
-from zeep.transports import AsyncTransport
 
 from service.soap_client.format_error import build_error_response
 from service.soap_client.wsdl.wsdl_manager import get_wsaa_wsdl
@@ -14,23 +12,18 @@ from service.utils.logger import logger
 
 afip_wsdl = get_wsaa_wsdl()
 
-# Implement retries with tenacity only for these Exceptions.
 @retry(
         retry=retry_if_exception_type(( ConnectionResetError, httpx.ConnectError, TransportError )),
         stop=stop_after_attempt(3),
         wait=wait_fixed(0.5),
         before_sleep=before_sleep_log(logger, logging.WARNING),
     )
-async def login_cms(b64_cms: str) -> dict:
+async def consult_afip_wsaa(make_request, METHOD) -> dict:
 
     logger.info("Starting CMS login request to AFIP")
-    METHOD = "loginCms"
-    httpx_client = httpx.AsyncClient(timeout=30.0)
-    transport = AsyncTransport(client=httpx_client)
 
     try:
-        client = AsyncClient(wsdl=afip_wsdl, transport=transport)
-        login_ticket_response = await client.service.loginCms(b64_cms)
+        login_ticket_response = await make_request()
         logger.info("CMS login request to AFIP ended successfully.")
 
         return {
@@ -49,18 +42,12 @@ async def login_cms(b64_cms: str) -> dict:
         # often due to invalid input, datatype mismatches, or business rule violations. 
         # These errors are the caller's responsibility to handle.
 
-        logger.debug(f"SOAP FAULT in login_cms: {e}")
+        logger.debug(f"SOAP FAULT in {METHOD}: {e}")
         return build_error_response(METHOD, "SOAPFault", str(e))
     
     except XMLSyntaxError as e:
         return build_error_response(METHOD, "Invalid AFIP response", str(e))
 
     except Exception as e:
-        logger.error(f"General exception in login_cms: {e}")
+        logger.error(f"General exception in {METHOD}: {e}")
         return build_error_response(METHOD, "unknown", str(e))
-    
-    finally:
-        if client and client.transport:
-            await client.transport.aclose()
-        else:
-            await httpx_client.aclose()
