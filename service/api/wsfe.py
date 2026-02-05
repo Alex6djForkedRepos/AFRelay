05c23be3c48a2c556fd50abc4f017a229b063399
Nehuen Lian <nehuenn.lian@gmail.com>
2026-02-05 16:39:40 -0300

    remove controllers for WSFE operations and unify controller and endpoint logic

--- service/api/wsfe.py
+++ service/api/wsfe.py
@@ -2,45 +2,69 @@ from fastapi import APIRouter, Depends
 
 from service.api.models.fecae_solicitar import RootModel
 from service.api.models.invoice_query import InvoiceBase, InvoiceQueryRequest
-from service.controllers.consult_invoice_controller import \
-    consult_specific_invoice
-from service.controllers.request_invoice_controller import \
-    request_invoice_controller
-from service.controllers.request_last_authorized_controller import \
-    get_last_authorized_info
+from service.payload_builder.builder import (add_auth_to_payload, build_auth,
+                                             build_fecomp_req)
+from service.soap_client.async_client import WSFEClientManager
+from service.soap_client.wsdl.wsdl_manager import get_wsfe_wsdl
+from service.soap_client.wsfe import consult_afip_wsfe
 from service.utils.jwt_validator import verify_token
 from service.utils.logger import logger
+from service.xml_management.xml_builder import extract_token_and_sign_from_xml
 
 router = APIRouter()
+afip_wsdl = get_wsfe_wsdl()
+
 
 @router.post("/wsfe/FECAESolicitar")
 async def generate_invoice(sale_data: RootModel, jwt = Depends(verify_token)) -> dict:
     
-    logger.info("Received request to generate invoice at /wsfe/invoices")
+    logger.info("Received request to generate invoice at /wsfe/FECAESolicitar")
 
     sale_data = sale_data.model_dump()
-    invoice_result = await request_invoice_controller(sale_data)
+    token, sign = extract_token_and_sign_from_xml()
+    invoice_with_auth = add_auth_to_payload(sale_data, token, sign)
 
+    async def make_request():
+        manager = WSFEClientManager(afip_wsdl)
+        client = manager.get_client()
+        return await client.service.FECAESolicitar(invoice_with_auth['Auth'], invoice_with_auth['FeCAEReq'])
+
+    invoice_result = await consult_afip_wsfe(make_request, "FECAESolicitar")
     return invoice_result
 
 
 @router.post("/wsfe/FECompUltimoAutorizado")
-async def last_authorized(comp_info: InvoiceBase, jwt = Depends(verify_token)) -> dict:
+async def last_authorized(sale_data: InvoiceBase, jwt = Depends(verify_token)) -> dict:
+    logger.info("Received request to generate invoice at /wsfe/FECompUltimoAutorizado")
 
-    logger.info("Received request to fetch last authorized invoice at /wsfe/invoices/last-authorized")
+    sale_data = sale_data.model_dump()
+    ptovta = sale_data["PtoVta"]
+    cbtetipo = sale_data["CbteTipo"]
 
-    comp_info = comp_info.model_dump()
-    last_authorized_info = await get_last_authorized_info(comp_info)
+    auth = build_auth(sale_data)
 
+    async def make_request():
+        manager = WSFEClientManager(afip_wsdl)
+        client = manager.get_client()
+        return await client.service.FECompUltimoAutorizado(auth, ptovta, cbtetipo)
+
+    last_authorized_info = await consult_afip_wsfe(make_request, "FECompUltimoAutorizado")
     return last_authorized_info
 
 
 @router.post("/wsfe/FECompConsultar")
 async def consult_invoice(comp_info: InvoiceQueryRequest, jwt = Depends(verify_token)) -> dict:
-
-    logger.info("Received request to query specific invoice at /wsfe/invoices/query")
+    logger.info("Received request to query specific invoice at /wsfe/FECompConsultar")
 
     comp_info = comp_info.model_dump()
-    result = await consult_specific_invoice(comp_info)
+    fecomp_req = build_fecomp_req(comp_info)
 
+    auth = build_auth(comp_info)
+
+    async def make_request():
+        manager = WSFEClientManager(afip_wsdl)
+        client = manager.get_client()
+        return await client.service.FECompConsultar(auth, fecomp_req)
+
+    result = await consult_afip_wsfe(make_request, "FECompConsultar")
     return result
