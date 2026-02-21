2f03d875da464470c88b30dd4a409c86022ceae6
Nehuen Lian <nehuenn.lian@gmail.com>
2026-02-21 01:38:04 -0300

    feat: add response model for loginCms (WSAA) endpoint.

--- service/api/wsaa.py
+++ service/api/wsaa.py
@@ -1,13 +1,18 @@
 from fastapi import APIRouter, Depends
+from pydantic import BaseModel, Field
 
 from service.controllers.request_access_token_controller import \
     generate_afip_access_token
 from service.utils.jwt_validator import verify_token
 from service.utils.logger import logger
 
+
+class loginCmsResponse(BaseModel):
+    status: str = Field(..., example="success")
+
 router = APIRouter()
 
-@router.post("/wsaa/loginCms")
+@router.post("/wsaa/loginCms", response_model=loginCmsResponse)
 async def renew_access_token(jwt = Depends(verify_token)) -> dict:
     
     logger.info("Received request to generate invoice at /wsfe/invoices")
