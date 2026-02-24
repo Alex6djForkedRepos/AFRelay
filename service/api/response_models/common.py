1323ddd2b16df950813639b39c1941ef3fb708c4
Nehuen Lian <nehuenn.lian@gmail.com>
2026-02-24 03:43:36 -0300

    refactor: remove FECAEAGet and FECAEAGetResponse classes.
    - remove unused classes

--- service/api/response_models/common.py
+++ service/api/response_models/common.py
@@ -31,29 +31,6 @@ class FECAEASinMov(BaseModel):
     FchProceso: str | None = None
     PtoVta: int
 
-class FECAEAGet(BaseModel):
-
-    model_config = ConfigDict(populate_by_name=True)
-
-    CAEA: str | None = None
-    Periodo: int
-    Orden: int        
-    FchVigDesde: str | None = None
-    FchVigHasta: str | None = None
-    FchTopeInf: str | None = None
-    FchProceso:  str | None = None
-    
-    observaciones: Observaciones | None = Field(None, alias="Observaciones")
-
-class FECAEAGetResponse(BaseModel):
-
-    model_config = ConfigDict(populate_by_name=True)
-
-    fecaea_get: FECAEAGet | None = Field(None, alias="FECAEAGet")
-
-    events: Events | None = Field(None, alias="Events")
-    errors: Errors | None = Field(None, alias="Errors")
-
 ######### Infrastructure #########
 class ErrorDetails(BaseModel):
     method: str
