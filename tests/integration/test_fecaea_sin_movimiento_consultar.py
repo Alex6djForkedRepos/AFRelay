070a56eefb72dbb7a58ec25487fc5b0d6c2fb1b4
Nehuen Lian <nehuenn.lian@gmail.com>
2026-03-15 06:21:59 -0300

    test: add integration test for FECAEASinMovimientoConsultar

---
+++ tests/integration/test_fecaea_sin_movimiento_consultar.py
@@ -0,0 +1,29 @@
+import pytest
+from httpx import AsyncClient
+
+from .soap_responses import FECAEASinMovimientoConsultarResponse
+
+
+@pytest.mark.asyncio
+async def test_fecaea_sin_movimiento_consultar_success(client: AsyncClient, wsfe_httpserver_fixed_port, wsfe_manager, override_auth):
+
+    # Configure http server
+    wsfe_httpserver_fixed_port.expect_request("/soap", method="POST").respond_with_data(
+        FECAEASinMovimientoConsultarResponse, content_type="text/xml"
+    )
+
+    # Payload
+    payload = {
+        "Auth": {
+            "Cuit": 30740253022
+        },
+        "CAEA": "00000000000000",
+        "PtoVta": 1
+    }
+
+    # Fastapi endpoint call
+    resp = await client.post("/wsfe/FECAEASinMovimientoConsultar", json=payload)
+
+    assert resp.status_code == 200
+    data = resp.json()
+    assert data["status"] == "error" # TODO: Update assertion once WSDL inconsistencies are resolved; currently expecting 'error'.
