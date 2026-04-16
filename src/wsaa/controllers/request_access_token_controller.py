3e5a7f0ab3e37ac615f178b240555a78e4acbf8e
Nehuen Lian <nehuenn.lian@gmail.com>
2026-04-16 18:33:49 -0300

    refactor: remove login_cms function and client initialization. use function to build xml

--- src/wsaa/controllers/request_access_token_controller.py
+++ src/wsaa/controllers/request_access_token_controller.py
@@ -1,12 +1,11 @@
 from src.shared.paths_config.paths import get_as_bytes
 from src.shared.utils.logger import logger
 from src.wsaa.crypto.sign import sign_login_ticket_request
-from src.wsaa.soap_client.client_manager import wsaa_client
 from src.wsaa.soap_client.wsaa import consult_afip_wsaa
-from src.wsaa.soap_client.wsdl.wsdl_manager import get_wsaa_wsdl
 from src.wsaa.time.time_management import generate_ntp_timestamp
 from src.wsaa.xml_management.xml_builder import (
-    build_login_ticket_request, parse_and_save_loginticketresponse, save_xml)
+    build_login_ticket_request, build_xml_to_send,
+    parse_and_save_loginticketresponse, save_xml)
 
 
 async def generate_afip_access_token() -> dict:
@@ -17,21 +16,9 @@ async def generate_afip_access_token() -> dict:
     save_xml(root, "loginTicketRequest.xml")
     login_ticket_request_bytes, private_key_bytes, certificate_bytes = get_as_bytes()
     b64_cms = sign_login_ticket_request(login_ticket_request_bytes, private_key_bytes, certificate_bytes)
+    wsaa_xml = build_xml_to_send(b64_cms)
 
-    afip_wsdl = get_wsaa_wsdl()
-    client, httpx_client = wsaa_client(afip_wsdl)
-
-    async def login_cms():
-        try:
-            return await client.service.loginCms(b64_cms)
-
-        finally:
-            if client and client.transport:
-                await client.transport.aclose()
-            else:
-                await httpx_client.aclose()
-
-    login_ticket_response = await consult_afip_wsaa(login_cms, "loginCms")
+    login_ticket_response = await consult_afip_wsaa(wsaa_xml)
 
     if login_ticket_response["status"] == "success":
         parse_and_save_loginticketresponse(login_ticket_response["response"], save_xml)
